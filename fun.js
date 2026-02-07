59b1ddbaba4a957f059d2267cae5d070cca04128
Dreacel <143748787+Dreacel@users.noreply.github.com>
2026-02-06 23:27:54 -0600

    it works i think

--- fun.js
+++ fun.js
@@ -1,5 +1,39 @@
-function sayHello(name) {
-  return "Hello " + name;
+const name= "Sam"
+
+function toUpper (text) {
+    const upperCased = text.toUpperCase();
+    console.log (upperCased);
 }
 
-let greeting = sayHello("Sam");
+toUpper(name);
+
+function Multiply(a,b){
+  console.log(a*b);
+
+}
+
+
+let results = Multiply (3,3);
+
+function Multiply(a,b){
+  console.log(a*b);
+
+}
+
+
+let results01 = Multiply (4,5);
+
+function Multiply(a,b){
+  console.log(a*b);
+
+}
+
+
+let results02 = Multiply (10,10);
+
+function logger(){
+    console.log("3 multiply problems") 
+    console.log("3 addition problems")
+}
+
+logger();
